5d6a22a0f752f34aa774415c4fcc58e836964065
RyanTan <ryantan0105@gmail.com>
2020-05-13 11:36:20 +0800

    RYAN-2 to be reviewed.

--- task2/screens/reviewDetails.js
+++ task2/screens/reviewDetails.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Image } from 'react-native';
 
 export default function ReviewDetails(props){
     const {navigation,route} = props
@@ -7,6 +7,8 @@ export default function ReviewDetails(props){
         <View style={styles.container}>
             <Text>ReviewDetails Screen</Text>
             <Text>{route.params.name}</Text>
+            <Image source={{uri: 'https://dwxbwps5boihg.cloudfront.net/topic/post_picture/11568/cafe_1200x628_large.jpg'}}
+       style={{width: 400, height: 400}} />
         </View>
     );
 }
